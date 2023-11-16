99b729b24be139b8e849896480e8b5d2495db2f9
Hasib <hasibfire24@gmail.com>
2023-11-16 19:15:25 +0600

    finished work section.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Hero from "./Hero/Hero";
 import Navbar from "./NavBar/Navbar";
+import Work from "./Work/Work";
 
 function App() {
   return (
     <div className="flex flex-col relative">
       <Navbar/>
       <Hero/>
+      <Work/>
     </div>
   );
 }
