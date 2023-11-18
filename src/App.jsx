0f34ca14523ac4c6359f5887b47099a6ec3b2593
Hasib <hasibfire24@gmail.com>
2023-11-18 16:50:21 +0600

    finished writing the codes. now to debug

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import "./App.css";
+import Footer from "./Footer/Footer";
+import ShopBtn from "./Footer/ShopBtn";
 import Hero from "./Hero/Hero";
 import Navbar from "./NavBar/Navbar";
 import Work from "./Work/Work";
+import Contact from "./contact/Contact";
 
 function App() {
   return (
@@ -9,6 +12,9 @@ function App() {
       <Navbar/>
       <Hero/>
       <Work/>
+      <Contact/>
+      <Footer/>
+      <ShopBtn/>
     </div>
   );
 }
